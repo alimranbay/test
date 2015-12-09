7a827938ff9e5cd16b8bde118ef8fcf9a7dfaf95
Ali <alimranbay@gmail.com>
2015-12-09 16:57:02 +0100

    commit 2

--- src/test.java
+++ src/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Dit is een testje testje");
         System.out.println("lol");
+        System.out.println("EBOLA");
     }
 }
